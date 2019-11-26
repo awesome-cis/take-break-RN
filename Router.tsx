b65c2e8d2ec6b4f50a2061d4a013e079748b40c4
letsget23 <letsget23@gmail.com>
2019-11-26 20:17:15 +0900

    Add basic screens

---
+++ Router.tsx
@@ -0,0 +1,16 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import HomeScreen from './src/HomeScreen';
+import DetailsScreen from './src/DetailsScreen';
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
+
+export default createAppContainer(AppNavigator);
